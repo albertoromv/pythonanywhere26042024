e240f382b4340d26c8dc7fd478a4831477867ff1
Alberto <albertoromv@hotmail.com>
2024-04-26 20:09:53 +0200

    lele

--- app_model.py
+++ app_model.py
@@ -70,7 +70,7 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 def webhook():
     # Lelele
     
-    path_repo = '/home/albertoromv/clase26042024/pythonanywhere26042024'
+    path_repo = '/home/albertoromv/clase26042024/pythonanywhere26042024/'
     servidor_web = '/var/www/albertoromv_pythonanywhere_com_wsgi.py' 
 
     # Comprueba si la solicitud POST contiene datos JSON
