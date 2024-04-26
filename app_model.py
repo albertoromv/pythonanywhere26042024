1bbe56880ab4429128073ca800a7fc347c666e8d
Alberto <albertoromv@hotmail.com>
2024-04-26 19:16:32 +0200

    barra añadida

--- app_model.py
+++ app_model.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-path_base = "/home/albertoromv/clase26042024/pythonanywhere26042024"
+path_base = "/home/albertoromv/clase26042024/pythonanywhere26042024/"
 
 # Enruta la landing page (endpoint /)
 @app.route('/', methods=['GET'])
